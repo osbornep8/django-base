fbda72df505c152eba1f074ba8edb5a57de0b6d0
osbornep8 <pereiraosborne8@gmail.com>
2025-07-19 22:50:21 +0100

    Updated names

--- medai/urls.py
+++ medai/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for movies project.
+URL configuration for medai project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
