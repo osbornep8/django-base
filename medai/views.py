71547c1e5992d0fb219dd691a2ac5d0ad0dee637
osbornep8 <pereiraosborne8@gmail.com>
2025-07-20 23:43:40 +0100

    Test template and view

--- medai/views.py
+++ medai/views.py
@@ -3,18 +3,30 @@ from django.shortcuts import render
 
 
 # procees JSON variables with leading underscores within the view itself and store it to be called elsewhere
-metadata = { "metadata_1":{
-  "_id": {
-    "$oid": "67c95e5c5cfd98ba96e179cb"
-  },
+data = { 
+    "metadata":[
+        {
   "subject_id": "002_S_0295",
   "study_uid": "3566",
   "series_uid": "S13402",
-  "image_uid": "I13712"
-}}
+  "image_uid": "I13712",
+}, 
+{
+  "subject_id": "002_S_0295",
+  "study_uid": "3566",
+  "series_uid": "S13402",
+  "image_uid": "I13713",
+},
+{
+  "subject_id": "002_S_0295",
+  "study_uid": "5726",
+  "series_uid": "S21853",
+  "image_uid": "I28556",
+},
+]}
 
 def metadata(request):
-    return render(request, "metadata/metadata.html", metadata)
+    return render(request, "metadata/metadata.html", data)
 
 def project_info(request):
     return HttpResponse("ADNI Project")
